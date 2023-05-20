df933ed6216a617c546b688cdd99d371b2b94d5a
Marat <125448969+martini69lol@users.noreply.github.com>
2023-05-20 15:13:37 +0300

    Update individual1.py

--- individual1.py
+++ individual1.py
@@ -1,10 +1,10 @@
 from myfunctions import make_shab
 
 def zamik():
-f = input('Фамилия: ')
-n = input('Имя: ')
-result = make_shab(f, n)
-print(result)
+    f = input('Фамилия: ')
+    n = input('Имя: ')
+    result = make_shab(f, n)
+    print(result)
 
 if name == "main":
-zamik()
+    zamik()
